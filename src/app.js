29587993901a38b4377f92d543bcc3e09685dd95
Santiago Zamora <szamorag@unal.edu.co>
2026-02-16 11:31:32 -0500

    Ejercicio version 1.0.0

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.json({ message: 'Hola, DevOps!' });
+});
+app.get('/health', (req, res) => {
+res.json({ status: 'OK', timestamp: new Date() });
+});
+app.get('/version', (req, res) => {
+res.json({ version: '1.0.0' });
+});
+
+if (require.main !== module) {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log( "Server running on port ${PORT} ");
+});
+}
+module.exports = app;
