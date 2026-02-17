1ae56cee60926b5331efb3d7b662ed7a83f61970
Nicolás <durregob@unal.edu.co>
2026-02-16 23:40:18 -0500

    Fase 2: se corrige codigo de app.js se agrega test de /version, gitignore y workflow de test

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-res.json({ message: 'Hola, DevOps!' });
+ res.json({ message: 'Hola, DevOps!' });
 });
 app.get('/health', (req, res) => {
-res.json({ status: 'OK', timestamp: new Date() });
+ res.json({ status: 'OK', timestamp: new Date() });
 });
 app.get('/version', (req, res) => {
-res.json({ version: '1.0.0' });
+    res.json({ version: '1.0.0' }); 
 });
-
-if (require.main !== module) {
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log( "Server running on port ${PORT} ");
-});
-}
 module.exports = app;
+if (require.main === module) {
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`);
+ });
+}
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,16 +1,19 @@
-const request = require('supertest'); 
-const app = require('../src/app'); 
- 
-describe('API Endpoints', () => { 
-  test('GET / should return welcome message', async () => { 
-    const response = await request(app).get('/'); 
-    expect(response.status).toBe(200); 
-    expect(response.body.message).toBe('Hola, DevOps!'); 
-  }); 
- 
-  test('GET /health should return OK status', async () => { 
-    const response = await request(app).get('/health'); 
-    expect(response.status).toBe(200); 
-    expect(response.body.status).toBe('OK'); 
-  }); 
+const request = require('supertest');
+const app = require('../src/app');
+describe('API Endpoints', () => {
+ test('GET / should return welcome message', async () => {
+ const response = await request(app).get('/');
+ expect(response.status).toBe(200);
+ expect(response.body.message).toBe('Hola, DevOps!');
+ });
+ test('GET /health should return OK status', async () => {
+ const response = await request(app).get('/health');
+ expect(response.status).toBe(200);
+ expect(response.body.status).toBe('OK');
+ });
+ test('GET /version should return version', async () => {
+  const response = await request(app).get('/version');
+  expect(response.status).toBe(200);
+  expect(response.body.version).toBe('1.0.0'); 
+ });
 });
